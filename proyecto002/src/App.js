a6186b915972dafe996a6200fb05aeb57b631920
Gastón Marcilio <gaston.marcilio@gmail.com>
2020-11-27 00:50:34 -0300

    Initial commit

---
+++ proyecto002/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {this.mostrarTitulo('Hola Mundo')}
+        {this.mostrarTitulo('Fin')}        
+      </div>
+    );
+  }
+
+  mostrarTitulo(tit) {
+    return (<h1>
+             {tit}
+            </h1>)
+  }
+
+}
+
+export default App;
